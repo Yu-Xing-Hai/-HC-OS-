c1ca1d592c74391cac414d7bcb1cd2f0f2eff758
宇星海 <1985375083@qq.com>
2025-01-02 16:28:01 +0800

    2025/1/2 第一次回滚
    代码发生GP异常，通过回滚查看问题源头。

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -24,20 +24,6 @@ static struct gate_desc idt[IDT_DESC_CNT]; //it is an array of interrupt descrip
 
 extern intr_handler intr_entry_table[IDT_DESC_CNT];  //it's also an array of "address of each interrupt handler",and the type of "intr_handle" is defined in document which is named "interrupt.h".
 
-char* intr_name[IDT_DESC_CNT];
-intr_handler idt_table[IDT_DESC_CNT];
-
-static void general_intr_handler(uint8_t vec_nr) {
-   if(vec_nr == 0x27 || vec_nr == 0x2f) {
-      return;
-   }
-   put_str("int vector : 0x");
-   put_int(vec_nr);
-   put_char(' ');
-   put_str(intr_name[vec_nr]);
-   put_char('\n');
-}
-
 /*Create interrupt gate descriptor*/
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function) {
    p_gdesc->func_offset_low_word = (uint32_t)function & 0x0000FFFF; //function: address of each interrupt handler.
@@ -45,34 +31,6 @@ static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler 
    p_gdesc->dcount = 0;
    p_gdesc->attribute = attr;
    p_gdesc->func_offset_high_word = ((uint32_t)function & 0xFFFF0000) >> 16;
-}
-
-static void exception_init(void) {
-   int i;
-   for(i = 0; i < IDT_DESC_CNT; i++) {
-      idt_table[i] = general_intr_handler;
-      intr_name[i] = "unknow";
-   }
-   intr_name[0] = "#DE Divide Error";
-   intr_name[1] = "#DB Debug Exception";
-   intr_name[2] = "NMI Interrupt";
-   intr_name[3] = "BP Breakpoint Exception";
-   intr_name[4] = "OF Overflow Exception";
-   intr_name[5] = "BR BOUND Range Exceeded Exception";
-   intr_name[6] = "UD Invalid Opcode Exception";
-   intr_name[7] = "NM Device Not Available Exception";
-   intr_name[8] = "DF Double Fault Exception";
-   intr_name[9] = "Coprocessor Segement Overrun";
-   intr_name[10] = "TS Invalid TSS Exception";
-   intr_name[11] = "NP Segement Not Present";
-   intr_name[12] = "SS Stack Fault Exception";
-   intr_name[13] = "GP General Protection Exception";
-   intr_name[14] = "PF Page-Faule Exception";
-   //intr_name[15] this is holds.
-   intr_name[16] = "MF x87 fpu Floating-Point Error";
-   intr_name[17] = "AC Alignment Check Exception";
-   intr_name[18] = "MC Machine-Check Exception";
-   intr_name[19] = "XF SIMD Floation-Point Exception";
 }
 
 /*Initialize IDT*/
@@ -109,7 +67,6 @@ static void pic_init(void) {
 /*Finish all initial work about interrupt*/
 void idt_init() {
    put_str("idt_init start\n");
-   exception_init();
    idt_desc_init();
    pic_init();
 
